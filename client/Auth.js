bc0926dc6f3992c0f3ef62ab301f2e617f4884e2
Faris Chtatou <faris.manage@gmail.com>
2021-06-18 00:56:26 +0200

    end_point inside constructor

--- client/Auth.js
+++ client/Auth.js
@@ -1,26 +1,31 @@
 import axios from "axios"
-import API_URL from "./config.json"
-
-const END_POINT = "auth"
+import { API_URL } from "./config.json"
 
 class AuthApi {
+  constructor() {
+    this.END_POINT = "auth"
+  }
+
   async register(userInformations) {
     return await axios.post(
-      `${API_URL}/${END_POINT}/register`,
+      `${API_URL}/${this.END_POINT}/register`,
       userInformations
     )
   }
 
   async login(userCredentials) {
-    return await axios.post(`${API_URL}/${END_POINT}/login`, userCredentials)
+    return await axios.post(
+      `${API_URL}/${this.END_POINT}/login`,
+      userCredentials
+    )
   }
 
   async getAuthenticatedUser() {
-    return await axios.get(`${API_URL}/${END_POINT}/me`)
+    return await axios.get(`${API_URL}/${this.END_POINT}/me`)
   }
 
   async logout() {
-    return await axios.post(`${API_URL}/${END_POINT}/logout`)
+    return await axios.post(`${API_URL}/${this.END_POINT}/logout`)
   }
 }
 
