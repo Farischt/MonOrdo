c93eb354e10bec14d12ca10569b42bcd4cdddce6
Faris Chtatou <faris.manage@gmail.com>
2021-06-18 00:59:05 +0200

    login error handling and ssp

--- pages/auth/index.jsx
+++ pages/auth/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useRouter } from "next/router"
 
+import Layout from "@/components/layout"
 import AuthApi from "@/client/Auth"
 
 export default function LoginPage() {
@@ -23,6 +24,7 @@ export default function LoginPage() {
   }
 
   const handleSubmit = async (event) => {
+    event.preventDefault()
     setLoading(true)
 
     try {
@@ -31,28 +33,63 @@ export default function LoginPage() {
       router.push("/")
     } catch (error) {
       setLoading(false)
-      setError("Une erreur est survenu")
+      switch (error.response.data.error) {
+        case "missing_email":
+          setError("Your email is missing !")
+          break
+        case "missing_password":
+          setError("Your password is missing !")
+          break
+        case "invalid_credentials":
+          setError("Invalid credentials !")
+          break
+        default:
+          setError("An unknow error occured !")
+      }
     }
   }
 
   return (
-    <form method="POST" onSubmit={handleSubmit}>
-      <h1> Login </h1>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={userCredentials.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={userCredentials.password}
-        onChange={handleChange}
-      />
-      <button type="submit"> Submit </button>
-    </form>
+    <Layout>
+      <form method="POST" onSubmit={handleSubmit}>
+        <h1> Login </h1>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={userCredentials.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={userCredentials.password}
+          onChange={handleChange}
+        />
+        <button type="submit"> Submit </button>
+        {error && <p> {error} </p>}
+        {loading && <p> Loading... </p>}
+      </form>
+    </Layout>
   )
+}
+
+import Backend from "@/server/index"
+
+export const getServerSideProps = async (context) => {
+  if (await Backend.getAuthenticatedUser(context)) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      success: true,
+    },
+  }
 }
