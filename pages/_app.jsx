ad802637b2d0438762dfd390ca837afad06a80ff
Faris Chtatou <faris.manage@gmail.com>
2021-06-18 00:57:29 +0200

    deleted generic layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,5 @@
-import Layout from "@/components/layout"
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
