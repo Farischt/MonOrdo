b66e9154344e301715ab6a497918566b8446d451
Faris Chtatou <faris.manage@gmail.com>
2021-06-26 00:57:48 +0200

    passwords methods

--- client/Auth.js
+++ client/Auth.js
@@ -27,6 +27,21 @@ class AuthApi {
   async logout() {
     return await axios.post(`${API_URL}/${this.END_POINT}/logout`)
   }
+
+  //! PASSWORD
+
+  async passwordResetRequest(email) {
+    return await axios.post(`${API_URL}/${this.END_POINT}/password/request`, {
+      email,
+    })
+  }
+
+  async passwordResetConfirm(credentials) {
+    return await axios.patch(
+      `${API_URL}/${this.END_POINT}/password/confirm`,
+      credentials
+    )
+  }
 }
 
 export default new AuthApi()
