33864f0fb276c37819da23167bf80736a03f3f0f
Faris Chtatou <faris.manage@gmail.com>
2021-07-01 12:17:27 +0200

    doctor registration mails

--- server/mails/index.js
+++ server/mails/index.js
@@ -76,6 +76,57 @@ class EmailService {
       console.log("Password reset confirmation email not sent ! ")
     }
   }
+
+  // ! Doctor Registration
+
+  async sendDoctorRegistrationConfirmation(to, first_name) {
+    try {
+      await this.transporter.sendMail({
+        from: CREDENTIALS.APP_EMAIL,
+        to,
+        subject: "Votre inscription en tant que médecin !",
+        text: "Votre inscription en tant que médecin !",
+        html: `<div> <h1> Merci pour votre inscription ${first_name} </h1>
+          <p> Nous avons bien enregistré votre inscription en tant que médecin ! Nous allons désormais procéder à une vérification de votre statut. Une fois cette vérification faite, un mail vous sera envoyé et vous pourrez utiliser toutes les fonctionnalités de MonOrdo grâce aux identifiants que vous avez saisi lors de votre inscription. Cette vérification peut prendre jusqu'à 48h, alors pas de panique !</p>
+        </div>`,
+      })
+    } catch (error) {
+      console.log("Doctor registration confirmation email not sent !")
+      console.log(error)
+    }
+  }
+
+  async sendDoctorVerified(to, first_name) {
+    try {
+      await this.transporter.sendMail({
+        from: CREDENTIALS.APP_EMAIL,
+        to,
+        subject: "Votre compte médecin est validé !",
+        text: "Votre compte médecin est validé !",
+        html: `<div> <h1> Félicitation, votre compte est validé ! </h1> 
+         <p> Bonjour ${first_name} ! Nous vérifié vos informations avec succès ! Vous pouvez désormais profiter des fonctionnalités de MonOrdo sans aucune restriction ! Rendez-vous sur notre site pour plus d'information ! </p>
+        </div>`,
+      })
+    } catch (error) {
+      console.log("Doctor registration verified email not sent !")
+    }
+  }
+
+  async sendAdminDoctorRegistration(to, first_name, last_name, rpps) {
+    try {
+      await this.transporter.sendMail({
+        from: CREDENTIALS.APP_EMAIL,
+        to,
+        subject: "Un nouveau médecin en attente de vérification !",
+        text: "Un nouveau médecin en attente de vérification !",
+        html: `<div> <h1> Et un de plus ! </h1> 
+          <p> Le médecin ${first_name} ${last_name} titulaire du rpps ${rpps} est en attente de vérification ! Rendez-vous sur votre dashboard administrateur afin de procéder à la vérification ! </p> 
+        </div>`,
+      })
+    } catch (error) {
+      console.log("Admin doctor registration email not sent !")
+    }
+  }
 }
 
 export default new EmailService()
