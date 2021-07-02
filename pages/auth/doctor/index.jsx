7d889c29ea2a8660c3a7d356f65beb9fa787e0a5
Faris Chtatou <faris.manage@gmail.com>
2021-07-02 19:08:28 +0200

    login pages

--- pages/auth/doctor/index.jsx
+++ pages/auth/doctor/index.jsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 
-import AdminApi from "@/client/Admin"
+import DoctorApi from "@/client/Doctor"
 import Layout from "@/components/layout"
 
-export default function AdminLoginPage({}) {
+export default function doctorLoginPage({}) {
   const router = useRouter()
 
   useEffect(() => {
-    router.prefetch("/admin")
+    router.prefetch("/doctor")
   }, [])
 
-  const [admin, setAdmin] = useState({
+  const [doctor, setDoctor] = useState({
     email: "",
     password: "",
   })
@@ -20,16 +20,19 @@ export default function AdminLoginPage({}) {
 
   const handleChange = (event) => {
     if (error) setError("")
-    setAdmin((state) => ({ ...state, [event.target.name]: event.target.value }))
+    setDoctor((state) => ({
+      ...state,
+      [event.target.name]: event.target.value,
+    }))
   }
 
   const handleSubmit = async (event) => {
     event.preventDefault()
     setLoading(true)
     try {
-      await AdminApi.login(admin)
+      await DoctorApi.login(doctor)
       setLoading(false)
-      router.push("/admin")
+      router.push("/doctor")
     } catch (error) {
       setError(error.message)
       setLoading(false)
@@ -39,19 +42,19 @@ export default function AdminLoginPage({}) {
   return (
     <Layout user={null}>
       <form method="POST" onSubmit={handleSubmit}>
-        <h1> Connexion Admin </h1>
+        <h1> Connexion Docteur </h1>
         <input
           type="email"
           name="email"
           placeholder="Email"
-          value={admin.email}
+          value={doctor.email}
           onChange={handleChange}
         />
         <input
           type="password"
           name="password"
           placeholder="Mot de passe"
-          value={admin.password}
+          value={doctor.password}
           onChange={handleChange}
         />
         {loading && <p> Chargement... </p>}
